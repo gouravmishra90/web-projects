491e36dea04c7b09e9d1883b6b86c424eaf8f323
Gourav Mishra <gouravmishra90@gmail.com>
2025-02-11 22:17:33 +0530

    Adding Standard Calculator JS Changes

---
+++ second-website/javaScriptsFiles/first-js.js
@@ -0,0 +1,16 @@
+function turn_Red(){
+    document.getElementById('Cell1').style.backgroundColor = 'red'
+    alert("Hello, there!")
+}
+
+function turn_Green(){
+    document.getElementById('Cell2').style.backgroundColor = 'green'
+}
+
+function turn_Yellow(){
+    document.getElementById('Cell3').style.backgroundColor = 'yellow'
+}
+
+function turn_Blue(){
+    document.getElementById('Cell4').style.backgroundColor = 'blue'
+}
